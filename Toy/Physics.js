d629b530af8c5c42929121a3ddc3231de9b300ba
Dougie <61299267+DougieMcGuire@users.noreply.github.com>
2025-03-09 12:34:22 -0500

    Update Physics.js

--- Toy/Physics.js
+++ Toy/Physics.js
@@ -1,15 +1,16 @@
-export class Physics {
-    constructor(gravity = 0.5) {
-        this.gravity = gravity;
+export class Player {
+    constructor(name) {
+        this.id = Math.random().toString(36).substring(7);
+        this.name = name;
+        this.x = window.innerWidth / 2;
+        this.y = window.innerHeight / 2;
+        this.speed = 5;
     }
 
-    applyGravity(character) {
-        character.y += this.gravity;
-    }
-
-    checkCollision(character, groundLevel) {
-        if (character.y > groundLevel) {
-            character.y = groundLevel;
-        }
+    move(keys) {
+        if (keys["w"]) this.y -= this.speed;
+        if (keys["s"]) this.y += this.speed;
+        if (keys["a"]) this.x -= this.speed;
+        if (keys["d"]) this.x += this.speed;
     }
 }
